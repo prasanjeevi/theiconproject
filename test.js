00ab780de729c513d345c0c4d86c166f744d7bdc
Sanjeevi <prasanjeevi@gmail.com>
2017-03-25 23:42:06 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var App = angular.module('App', [])
-
-App.controller('AppController', function($scope, $http){
-  $http.get('ip.min.json')
-    .then(function(res){
-      $scope.icons = res.data
-      $scope.count = Object.keys(res.data).reduce(function(a, b){ return a + res.data[b].length}, 0)
-    })
-})
