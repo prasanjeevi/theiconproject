ab85964d51152f02417dd8f86f5e5ed21c9ddf2c
Sanjeevi <prasanjeevi@gmail.com>
2017-03-25 23:40:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
 var App = angular.module('App', [])
 
-App.controller('AppController', function($scope){
-  $scope.icons = {
-  "glyphicons": [
-    "asterisk",
-    "plus",
-    "euro",
-    "eur"
-  ]
-  }
+App.controller('AppController', function($scope, $http){
+  $http.get('ip.min.json')
+    .then(function(res){
+      $scope.icons = res.data
+      $scope.count = Object.keys(res.data).reduce(function(a, b){ return a + res.data[b].length}, 0)
+    })
 })
