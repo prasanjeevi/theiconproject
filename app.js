8a1aa076df89c843059271ddcc1d1ecf54f06900
Sanjeevi <prasanjeevi@gmail.com>
2017-03-25 23:44:05 +0530

    arrow function not working in iPad

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var App = angular.module('App', [])
 
-App.controller('AppController', ($scope, $http) => {
+App.controller('AppController', function($scope, $http) {
   $http.get('ip.min.json')
-    .then(res => {
+    .then(function(res) {
       $scope.icons = res.data
-      $scope.count = Object.keys(res.data).reduce((a, b) => a + res.data[b].length, 0)
+      $scope.count = Object.keys(res.data).reduce(function(a, b) { return a + res.data[b].length }, 0)
     })
 })
