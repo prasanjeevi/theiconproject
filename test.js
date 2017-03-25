7fae1b83c33695f813f893131275e951632e4340
Sanjeevi <prasanjeevi@gmail.com>
2017-03-25 23:32:20 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 var App = angular.module('App', [])
 
 App.controller('AppController', ($scope, $http, $window) => {
-  $http.get('notfound.json')
-    .then(res => {
-      $scope.icons = res.data
-      $scope.count = Object.keys(res.data).reduce((a, b) => a + res.data[b].length, 0)
-      $window.alert(res.status);
-    }, function(res){$window.alert(res.statusText)})
+  $scope.icons = {
+  "glyphicons": [
+    "asterisk",
+    "plus",
+    "euro",
+    "eur"
+  ]
+  }
 })
