24500c2fafcc252b4815e9eb1af812a097e54e43
Sanjeevi <prasanjeevi@gmail.com>
2017-03-25 23:00:47 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+var App = angular.module('App', [])
+
+App.controller('AppController', ($scope, $http, $window) => {
+  $http.get('notfound.json')
+    .then(res => {
+      $scope.icons = res.data
+      $scope.count = Object.keys(res.data).reduce((a, b) => a + res.data[b].length, 0)
+      $window.alert(res.status);
+    }, function(){$window.alert(res.statusText)})
+})
